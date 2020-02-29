56fe0b205eae93158ca55b3f1c3587be1818696e
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2020-02-29 22:11:40 +0100

    Added styled components

--- src/component/Gallery/GalleryList.js
+++ src/component/Gallery/GalleryList.js
@@ -1,63 +1,61 @@
-import React, { useState, useCallback } from "react";
-import Gallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
-import { Container, Row, Col, Image } from "react-bootstrap";
+import React from 'react';
+import { Image } from 'react-bootstrap';
+import styled from 'styled-components';
+import Logo1 from '../../assets/images/logo1.png';
 
-import { photos } from "./photos";
-import Logo1 from "../../assets/images/logo1.png";
+const StyledWrapper = styled.div`
+  display: grid;
+  width: 70vw;
+  margin: 10px auto 70px auto;
+`;
+
+const StyledHeader = styled.div`
+  padding-top: 15px;
+  display: grid;
+  grid-template-columns: 0.09fr 0.75fr;
+`;
+
+const StyledGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-items: center;
+`;
 
 const GalleryList = () => {
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
-
-  const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
-
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
-
   return (
-    <Container>
-      <div style={{ width: "60vw", margin: "0 auto" }}>
-      <Col>
-        <div style={{ marginTop: 10, marginBottom: 10 }}>
-          <Row>
-            <Col sm={1}>
-              <Image src={Logo1} roundedCircle style={{ maxWidth: "70px" }} />
-            </Col>
-            <Col sm={11}>
-              <div style={{ marginLeft: 10}}>Jan Kowlaski Fotograf</div>
-              <div style={{ marginLeft: 10 }}>Historia Weselna | Kętrzyn | 2020</div>
-            </Col>
-          </Row>
+    <StyledWrapper>
+      <StyledHeader>
+        <div>
+          <Image src={Logo1} roundedCircle style={{ maxWidth: '70px', marginLeft: '10px' }} />
         </div>
-        <Row>
-          <div style={{ width: "60vw", margin: "0 auto" }}>
-            <Gallery photos={photos} onClick={openLightbox} />
-            <ModalGateway>
-              {viewerIsOpen ? (
-                <Modal onClose={closeLightbox}>
-                  <Carousel
-                    currentIndex={currentImage}
-                    views={photos.map(x => ({
-                      ...x,
-                      srcset: x.srcSet,
-                      caption: x.title
-                    }))}
-                  />
-                </Modal>
-              ) : null}
-            </ModalGateway>
-          </div>
-        </Row>
-        </Col>
+        <div>
+          <p style={{ paddingLeft: '10px' }}> Jan Kowlaski Fotograf</p>
+          <p style={{ paddingLeft: '10px' }}>Historia Weselna | Kętrzyn | 2020</p>
         </div>
-        
-    </Container>
+      </StyledHeader>
+      <StyledGrid>
+        <div>
+          <img src={Logo1} style={{ maxWidth: '350px' }} alt="galleryItem" />
+        </div>
+        <div>
+          <img src={Logo1} style={{ maxWidth: '350px' }} alt="galleryItem" />
+        </div>
+        <div>
+          <img src={Logo1} style={{ maxWidth: '350px' }} alt="galleryItem" />
+        </div>
+      </StyledGrid>
+      <StyledGrid>
+        <div>
+          <img src={Logo1} style={{ maxWidth: '350px' }} alt="galleryItem" />
+        </div>
+        <div>
+          <img src={Logo1} style={{ maxWidth: '350px' }} alt="galleryItem" />
+        </div>
+        <div>
+          <img src={Logo1} style={{ maxWidth: '350px' }} alt="galleryItem" />
+        </div>
+      </StyledGrid>
+    </StyledWrapper>
   );
 };
 
